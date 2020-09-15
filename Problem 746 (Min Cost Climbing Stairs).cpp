3e02c836c3e49c93bb930e571b8285527fcb41a3
Abhishek Panwar <44024188+abhishekpanwar971@users.noreply.github.com>
2020-09-15 19:37:14 +0530

    Problem 746 (Min Cost Climbing Stairs)

---
+++ Problem 746 (Min Cost Climbing Stairs).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        int ans[n];
+        ans[0]=cost[0];
+        ans[1]=cost[1];
+        for(int i=2;i<n;i++){
+            
+                ans[i]=cost[i]+min(ans[i-1],ans[i-2]);
+            
+        }
+        
+        return min(ans[n-1],ans[n-2]);
+    }
+};
